80f04c250e2bf9163b9d95a61f7043f8f6a739e4
Brunocarpi <brunocarpi385@gmail.com>
2025-05-06 11:03:54 -0300

    atualizações da aula 06_05

---
+++ arvore_generica/No.java
@@ -0,0 +1,29 @@
+//<T> indica que é algo genérico, nesse contesto a classe No é genérica
+
+public class No <T extends Comparable<T>> {
+    private T info;
+    private No<T> esquerda;
+    private No<T> direita;
+    public No (T info) {
+        this.info = info;
+    }
+    public No<T> getEsquerda() {
+        return esquerda;
+    }
+    public No<T> getDireita() {
+        return direita;
+    }
+    public void setEsquerda (No<T> esquerda) {
+        this.esquerda = esquerda;
+    }
+    public void setDireita (No<T> direita) {
+        this.direita = direita;
+    }
+    public T getInfo() {
+        return info;
+    }
+    @Override
+    public String toString() {
+        return info + "";
+    }
+}
